aca22e1269e7cd93db8707a4221de49ecbe5b9ff
Paul Tiedtke <PaulTiedtke@web.de>
2020-11-09 20:08:26 +0100

    fix indent (4 -> 2)

--- popup.js
+++ popup.js
@@ -1,43 +1,43 @@
 'use strict';
 (() => {
-    // Make privacy cookie available in iframe
-    chrome.cookies.get({
-        url: 'https://www.deepl.com',
-        name: 'privacySettings',
-    },
-    (cookie) => {
-        chrome.cookies.set({
-            url: 'https://www.deepl.com',
-            name: cookie.name,
-            value: cookie.value,
-            domain: cookie.domain,
-            path: cookie.path,
-            secure: cookie.secure,
-            httpOnly: cookie.httpOnly,
-            sameSite: 'no_restriction',
-            expirationDate: cookie.expirationDate,
-            storeId: cookie.storeId,
-        });
+  // Make privacy cookie available in iframe
+  chrome.cookies.get({
+    url: 'https://www.deepl.com',
+    name: 'privacySettings',
+  },
+  (cookie) => {
+    chrome.cookies.set({
+      url: 'https://www.deepl.com',
+      name: cookie.name,
+      value: cookie.value,
+      domain: cookie.domain,
+      path: cookie.path,
+      secure: cookie.secure,
+      httpOnly: cookie.httpOnly,
+      sameSite: 'no_restriction',
+      expirationDate: cookie.expirationDate,
+      storeId: cookie.storeId,
     });
+  });
 
-    // Get selected text and 
-    chrome.tabs.executeScript({
-        code: "window.getSelection().toString();"
-    }, async (selection) => {
-        const selectedText = selection[0];
-        let url = await generateUrl(selectedText);
-        if (selectedText !== "") {
-            document.getElementsByTagName("iframe")[0].setAttribute('src', url);
-            chrome.storage.sync.set({'lastText': selectedText }, () => {
-                console.log('Saved:' + selectedText);
-            });
-        } else {
-            chrome.storage.sync.get(['lastText'], async (result) => {
-                if (result.lastText !== "") {
-                    url = await generateUrl(result.lastText);
-                }
-                document.getElementsByTagName("iframe")[0].setAttribute('src', url);
-            });
+  // Get selected text and open deepl.com
+  chrome.tabs.executeScript({
+    code: "window.getSelection().toString();"
+  }, async (selection) => {
+    const selectedText = selection[0];
+    let url = await generateUrl(selectedText);
+    if (selectedText !== "") {
+      document.getElementsByTagName("iframe")[0].setAttribute('src', url);
+      chrome.storage.sync.set({'lastText': selectedText }, () => {
+        console.log('Saved:' + selectedText);
+      });
+    } else {
+      chrome.storage.sync.get(['lastText'], async (result) => {
+        if (result.lastText !== "") {
+          url = await generateUrl(result.lastText);
         }
-    });
+        document.getElementsByTagName("iframe")[0].setAttribute('src', url);
+      });
+    }
+  });
 })();
