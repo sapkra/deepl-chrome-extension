39177f5512bde420093e32e25067637dede225cc
Paul Tiedtke <PaulTiedtke@web.de>
2020-11-09 20:06:54 +0100

    use page_action instead browser_action

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
+// Register context menu
 chrome.contextMenus.onClicked.addListener(async ({ selectionText }) => {
   chrome.tabs.create({
     url: await generateUrl(selectionText),
@@ -10,3 +11,19 @@ chrome.runtime.onInstalled.addListener(() => chrome.contextMenus.create({
   contexts: ['selection'],
   title: 'Translate with DeepL "%s"'
 }));
+
+// Activate popup
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [
+        new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: {
+            hostContains: '.'
+          }
+        })
+      ],
+      actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+  });
+});
